a02bc410a08ae36a187a03e48780dac4ce38da74
Pawel Choniawko <pawel.choniawko@capgemini.com>
2020-01-28 11:10:45 +0100

    add socket io

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-export const server = () => ({})
+const app = require("express")()
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
+
+const PORT = 4000
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+  })
+})
+
+io.on("connection", socket => {
+  console.log("a user connected")
+})
+
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`)
+})
